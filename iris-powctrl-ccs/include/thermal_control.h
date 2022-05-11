917e26f8557f99e90fcb25ef090310512bdeb8e4
jaydenmckoy <jpmckoy7@gmail.com>
2022-05-10 20:33:41 -0500

    Branch for thermal control dev

---
+++ iris-powctrl-ccs/include/thermal_control.h
@@ -0,0 +1,15 @@
+/*
+ * thermal_control.h
+ *
+ *  Created on: May 10, 2022
+ *      Author: Jayden McKoy
+ */
+
+#ifndef INCLUDE_THERMAL_CONTROL_H_
+#define INCLUDE_THERMAL_CONTROL_H_
+
+
+
+
+
+#endif /* INCLUDE_THERMAL_CONTROL_H_ */
