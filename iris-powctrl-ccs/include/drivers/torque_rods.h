960a8b9f6c446ec3e7fac62de9882316f0f15ade
jaydenmckoy <55809887+jaydenmckoy@users.noreply.github.com>
2022-09-21 19:35:58 -0500

    Added torque rod driver from adcs code

---
+++ iris-powctrl-ccs/include/drivers/torque_rods.h
@@ -0,0 +1,20 @@
+
+
+#include "pwm_basic.h"
+
+/*
+Converts a requested dipole value to a PWM duty cycle value. This uses a linear approximation of the dipole test results found 
+at https://docs.google.com/spreadsheets/d/1HabvoySxrTA8_qCVNMNoQwynStzFnOvv/edit#gid=589234245
+
+The linear approximation has a slope of 0.04 Am^2 / V, therefore the voltage for a requested dipole, D, is V = D / 0.04
+
+The maximum dipole is +-0.2Am^2 at 5V
+*/
+
+#define DIPOLE_SLOPE 0.04
+
+#define MAX_DIPOLE 0.2
+
+#define MAX_VOLTAGE 5.0
+
+float dipoleToVoltage(float dipole);
---
+++ iris-powctrl-ccs/src/drivers/torque_rods.c
@@ -0,0 +1,25 @@
+
+#include "torque_rods.h"
+#include "pwm_basic.h"
+
+
+float dipoleToVoltage(float dipole) {
+
+    if(abs(dipole) > MAX_DIPOLE) {
+        return MAX_VOLTAGE * ((dipole >= 0) ? 1 : -1); //convert to proper sign with ternary
+    }
+
+    float reqVoltage = 0.0;
+    reqVoltage = dipole / DIPOLE_SLOPE;
+
+    if(abs(reqVoltage) > MAX_VOLTAGE) {
+        return MAX_VOLTAGE * ((reqVoltage >= 0) ? 1 : -1);
+    } else {
+        return reqVoltage;
+    }
+
+}
+
+int getSign(float val) {
+
+}
